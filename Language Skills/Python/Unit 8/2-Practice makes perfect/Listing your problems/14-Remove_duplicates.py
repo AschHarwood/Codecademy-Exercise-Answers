d831fa62a57405f235c2ac8df2a76359bced4ea3
AschHarwood <asch.harwood@gmail.com>
2016-03-08 16:45:59 -0500

    Update 14-Remove_duplicates.py

--- Language Skills/Python/Unit 8/2-Practice makes perfect/Listing your problems/14-Remove_duplicates.py
+++ Language Skills/Python/Unit 8/2-Practice makes perfect/Listing your problems/14-Remove_duplicates.py
@@ -1,6 +1,6 @@
-def remove_duplicates(x):
-    b = []
-    for a in x:
-        if a not in b:
-            b.append(a)
+def remove_duplicates(x): # defines function
+    b = [] #creates empty list
+    for a in x: #loops through each element in x
+        if a not in b: #checks if each element is in the new list b
+            b.append(a) # if not, adds to new list
     return b
